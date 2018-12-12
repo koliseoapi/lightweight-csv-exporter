0036b0c33cf5e9b727cef27b3acfdf473cad482f
Nacho Coloma <icoloma@gmail.com>
2018-12-12 23:06:32 +0100

    First commit

--- test/CsvTest.js
+++ test/CsvTest.js
@@ -2,7 +2,12 @@ import { createCsvExporter } from "../lib/main";
 
 describe("Exporter", () => {
   it("renders CSV", () => {
-    const csv = createCsvExporter();
+    const csv = createCsvExporter({
+      name: undefined,
+      age: undefined,
+      created: ({ created }) => created && created.toISOString(),
+      description: undefined
+    });
     const data = [
       { name: "Bond, James", age: 30, created: new Date("1953-04-13") },
       {
@@ -10,10 +15,14 @@ describe("Exporter", () => {
         age: 37,
         created: new Date("1895-01-01"),
         description:
-          "Emilio Salgari wrote several novels chronicling the adventures of Sandokan and Yanez, two of his most legendary creations.\n The pirates are introduced in The Tigers of Mompracem, which portrays their relentless struggle against the Dutch and British powers that seek to wipe them out. "
+          'Emilio Salgari wrote several novels chronicling the adventures of Sandokan and Yanez, two of his most legendary creations.\n The pirates are introduced in "The Tigers of Mompracem", which portrays their relentless struggle against the Dutch and British powers that seek to wipe them out. '
       },
-      { name: "Mortadelo y Filemón", age: 50.56667 }
+      { name: "Mortadelo y Filemón", age: 50.56667 },
+      null,
+      undefined
     ];
-    expect(csv.export(data)).toMatchSnapshot();
+    const output = csv.export(data);
+    console.log(output);
+    expect(output).toMatchSnapshot();
   });
 });
