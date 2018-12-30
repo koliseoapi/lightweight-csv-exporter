9427e1823cb5bd1268908a45ae9a0a60fa916041
Nacho Coloma <icoloma@gmail.com>
2018-12-30 18:23:49 +0100

    Fixed tests

---
+++ test/csv-test.js
@@ -0,0 +1,11 @@
+import createCsvExporter from "../lib/csv-exporter";
+import { config, data } from "./mock-data";
+
+describe("CSV Exporter", () => {
+  it("renders CSV", () => {
+    const csv = createCsvExporter(config);
+    const output = csv.export(data);
+    //    console.log(output);
+    expect(output).toMatchSnapshot();
+  });
+});
