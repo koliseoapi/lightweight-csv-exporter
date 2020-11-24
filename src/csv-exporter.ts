04316acbe566951c551a819fd59ac59c8e007439
Carlos Coloma <ccescribano@gmail.com>
2020-11-24 15:54:47 +0000

    Move to typescript

--- src/csv-exporter.ts
+++ src/csv-exporter.ts
@@ -2,40 +2,49 @@
 const EMPTY = "";
 const EOL = "\n";
 
-function isNullOrUndefined(object) {
+function isNullOrUndefined(object?: any) {
   return typeof object === "undefined" || object === null;
 }
 
 // if value is not 100% safe (may contain newline, double quotes, or commas)
 // put between quotes and escape any quote
-function escape(value) {
-  return typeof value !== "string" || !value.length || /^[a-z0-9.@\/+\- ?]+$/i.test(value)
+function escape(value?: any) {
+  return typeof value !== "string" ||
+    !value.length ||
+    /^[a-z0-9.@\/+\- ?]+$/i.test(value)
     ? value
     : '"' + value.replace(/"/g, '""') + '"';
 }
 
-function createExtractor(property) {
-  if (property.indexOf('.') === -1) {
-    return function (object) {
+function createExtractor<T>(property: string): (param: T) => string {
+  if (property.indexOf(".") === -1) {
+    return function (object?: any) {
       return object[property];
-    }
+    };
   }
-  const parts = property.split('.');
-  return function (object) {
+  const parts = property.split(".");
+  return function (object: any) {
     let result = object;
     let i = 0;
     while (!isNullOrUndefined(result) && i < parts.length) {
-      result = result[parts[i++]]
+      result = result[parts[i++]];
     }
     return result;
-  }
+  };
 }
 
-export default function createCsvExporter(config) {
-  const labels = [];
+interface ConfigEntry<T> {
+  property: string | ((param: T) => string);
+  label: string;
+}
+
+export default function createCsvExporter<T>(
+  config: (string | ConfigEntry<T>)[]
+) {
+  const labels: string[] = [];
   const columns = config.map((configEntry, index) => {
     let property, label;
-    if (typeof configEntry === 'string') {
+    if (typeof configEntry === "string") {
       label = property = configEntry;
     } else {
       property = configEntry.property;
@@ -47,8 +56,8 @@ export default function createCsvExporter(config) {
     labels.push(escape(label));
     // function that will retrieve the value of item[property]
     const extractPropertyFrom =
-      typeof property === 'function' ? property : createExtractor(property);
-    return function (object) {
+      typeof property === "function" ? property : createExtractor(property);
+    return function (object: T) {
       if (isNullOrUndefined(object)) return EMPTY;
       const value = extractPropertyFrom(object);
       return isNullOrUndefined(value) ? EMPTY : escape(value);
@@ -56,12 +65,12 @@ export default function createCsvExporter(config) {
   });
 
   return {
-    export(data = []) {
+    export(data: T[] = []) {
       let result = labels.join(",") + EOL;
-      data.forEach(object => {
-        result += columns.map(callback => callback(object)).join(",") + EOL;
+      data.forEach((object) => {
+        result += columns.map((callback) => callback(object)).join(",") + EOL;
       });
       return result;
-    }
+    },
   };
 }
